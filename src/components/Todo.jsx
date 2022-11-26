f0546f85e9ce9664841eb154358b22858760acdb
Marine <marine.lenoir@hotmail.fr>
2022-11-26 13:11:50 +0100

    Feat-Suppression-Taches-Effectuées

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+const Todo = props => {
 
-function Todo({newTasks}) {
+  const {newTasks} = props;
+
+  const [tasksList, setTasksList] = useState([newTasks])
+  console.log(tasksList)
+
+  useEffect(() => {
+    setTasksList([...newTasks])
+  }, [newTasks])
+
+  const deleteTask = (id) => {
+    const taskListIndex = tasksList.findIndex(e => e.id === id);
+    const newTasksIndex = newTasks.findIndex(e => e.id === id);
+    tasksList.splice(taskListIndex, 1);
+    newTasks.splice(newTasksIndex, 1);
+    setTasksList([...tasksList]);
+  }
 
   return(
     <div>
       <p>My Tasks</p>
-      {newTasks.map((newTasks, index) => {
+      {tasksList.map((taskList, index) => {
         return(
         <div key={index}>
-          <p key={newTasks.id}>{newTasks.value}</p>
+          <p key={taskList.id}>{taskList.value}</p>
+          <button onClick={() => deleteTask(taskList.id)}>X</button>
         </div>
         );
       })}
